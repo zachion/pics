14a302b0ef7af699c265b85518ca435b67a4a834
Zach Ioannou <zioannou77@gmail.com>
2020-01-06 17:53:59 +0200

    making SearchBar a controlled component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  state = {};
+  state = { term: '' };
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
