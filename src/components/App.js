e368bde688081d5481a4436f3ef72cfe22adc5bf
Zach Ioannou <zioannou77@gmail.com>
2020-01-07 23:54:37 +0200

    added api call to unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:
+          'Client-ID 3bdd2155e12ef7fce68a7f3bc783707a77f35c327de897ac7988899d8c8747fb'
+      }
+    });
   }
-  
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
