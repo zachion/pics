65d4554651c7d3d18df0d4a81508c967e527017b
Zach Ioannou <zioannou77@gmail.com>
2020-01-14 00:48:12 +0200

    added ImageList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
   state = { images: [] };
@@ -10,7 +11,6 @@ class App extends React.Component {
       params: { query: term }
     });
     this.setState({ images: response.data.results });
-    console.log(this.state.images);
   };
 
   render() {
@@ -18,6 +18,7 @@ class App extends React.Component {
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
         Found: {this.state.images.length}
+        <ImageList images={this.state.images} />
       </div>
     );
   }
